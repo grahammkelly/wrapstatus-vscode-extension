edc260d87235607de9692f1de7bf61d16d694b3c
734580 Graham Kelly <graham.kelly@travelport.com>
2022-11-29 12:30:10 +0000

    Displays the wrap column on wrap settings that this affects ('bounded' and 'wordWrapColumn') in both tooltip and the status bar itself.
    * Also removed notifications, to use console instead
    ** This meant a new import to allow me to use standard Typescript/node
    crap

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # wrapstatus-vscode-extension
 Adds a status bar item in VS Code (and VSCodium) to show current wrap status and allow changing without affecting settings
 
+This will display the current wrap setting on the status bar. For those wrap settings that are dependant on the setting defining the wrap column ('bounded', which is shortest of window boundary and the wrap column, or the 'wordWrapColumn').
+
 ## Acknowledgements
 This extension is based upon the sample statusbar from https://github.com/microsoft/vscode-extension-samples
 
--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,15 @@ import * as vscode from 'vscode';
 let statusBarItem: vscode.StatusBarItem;
 const extName = 'WordWrap status bar extension';
 const cmdId = 'statusbar.wordwrap.status';
+
 const wwSetting = "editor.wordWrap";
+const wrapColSetting = 'editor.wordWrapColumn';
+
+//
+//This caused me a lot of trouble!
+//  If you have trouble 'finding' any standard typescript/node functionality, make sure
+//  you have run `npm install @types/node --save-dev` at the command line
+//
 
 export function activate({subscriptions}: vscode.ExtensionContext) {
     //Register the commands to be invoked when the status bar item is selected 
@@ -16,27 +24,57 @@ export function activate({subscriptions}: vscode.ExtensionContext) {
     statusBarItem.command = cmdId;
     subscriptions.push(statusBarItem);
 
-    //Register a listener to make sure the status bar status is always up to date on the status bar
+    //Register a listener to make sure the status bar status is always up to date when the user changes the settings
     subscriptions.push(vscode.workspace.onDidChangeConfiguration(event => 
-        doActionForSettingChange(event, wwSetting, updateWrapStatusBar)));
+        doActionWhenSettingChangeDetected(event, [wwSetting, wrapColSetting], 
+            updateWrapStatusBarItem)));
 
-    vscode.window.showInformationMessage(`${extName}: Live status of the WordWrap setting will be shown in the status bar`);
+    console.log(`${extName}: Live status of the WordWrap setting will be shown in the status bar`)
     
-    updateWrapStatusBar();
+    updateWrapStatusBarItem();
 }
 
-function doActionForSettingChange(event: vscode.ConfigurationChangeEvent, settingToListenFor: string, action: () => any): void {
-    const isSettingWeWant = event.affectsConfiguration(settingToListenFor);
+function doActionWhenSettingChangeDetected(event: vscode.ConfigurationChangeEvent, settingToListenFor: string[], action: () => any): void {
+    const isSettingWeWant = settingToListenFor.some((setting:string) => event.affectsConfiguration(setting));
     
     if (isSettingWeWant) {
+        console.debug(`${extName}: Setting change detected`)
         action();
     }
 }
 
-function updateWrapStatusBar(): void {
-    const currentWrapStatus = vscode.workspace.getConfiguration().get(wwSetting);
-    statusBarItem.text = `Wrap: ${currentWrapStatus}`;
-    statusBarItem.tooltip = `Word-wrap is: ${currentWrapStatus}`
+function updateWrapStatusBarItem(): void {
+    const editorCfg = vscode.workspace.getConfiguration();
+    const currentWrapStatus = editorCfg.get(wwSetting) as string;
+    const wrapColumn = editorCfg.get(wrapColSetting);
+    statusBarItem.text = `Wrap: ${statusBarText()}`;
+   
+    statusBarItem.tooltip = statusBarToolTip();
 
     statusBarItem.show();
+
+    function statusBarToolTip(): string {
+        switch(currentWrapStatus) {
+            case "on":
+                return 'Lines will wrap at window boundary';
+            case "off":
+                return 'Lines will not wrap';
+            case 'wordWrapColumn':
+                return `Lines will wrap at column ${wrapColumn}`;
+            case 'bounded':
+                return `Lines will wrap at lesser of window boundary or column ${wrapColumn}`;
+        }
+        return '';
+    }
+
+    function statusBarText(): string {
+        switch (currentWrapStatus) {
+            case 'wordWrapColumn':
+                return `column(${wrapColumn})`;
+            case 'bounded':
+                return `bounded(${wrapColumn})`;
+        }
+        return currentWrapStatus;
+    }
 }
+
