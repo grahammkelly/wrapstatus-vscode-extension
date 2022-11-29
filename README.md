fb1de6cdfb0f4b3c4b8971c44bd5c4fa7e54abc0
734580 Graham Kelly <graham.kelly@travelport.com>
2022-11-29 15:29:44 +0000

    Updated extension to change the wordwrap status on double clicking the status bar item. Also updated README

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # wrapstatus-vscode-extension
 Adds a status bar item in VS Code (and VSCodium) to show current wrap status and allow changing without affecting settings
 
+When editing files, I tend to swap between the word wrapping as needed as I read a file. It's always bugged me that I need to open the settings editor and find the specific setting to change in this case instead of having some easy way to toggle the wrapping.
+
+I've allowed the setting to be set through double clicks on the status bar item, but the column to wrap on, I'm assuming you'll change less often so leaving that to be changed through the settings editor should be OK.
+
 This will display the current wrap setting on the status bar. For those wrap settings that are dependant on the setting defining the wrap column ('bounded', which is shortest of window boundary and the wrap column, or the 'wordWrapColumn').
+
+Also allows you to double click the status bar item, which will cycle through the wrap settings (in the order of 'on', 'off', 'wordWrapColumn', and 'bounded'). I'd prefer this to be in-memory only, and not stored, but unfortunately that doesn't seem possible in VSCode, so the changed setting is in the Workspace context. It will be 'remembered' through restarts only within the current workspace.
+
+## Supported versions
+This extension was tested on 
+* VSCodium 1.73.1
+* VisualStudio Code 
 
 ## Acknowledgements
 This extension is based upon the sample statusbar from https://github.com/microsoft/vscode-extension-samples
+The double click detection is basd on tips & code from `medo64` (from this repo - https://github.com/medo64/Code-Point)
 
 
--- src/extension.ts
+++ src/extension.ts
@@ -25,13 +25,54 @@ export function activate({subscriptions}: vscode.ExtensionContext) {
     subscriptions.push(statusBarItem);
 
     //Register a listener to make sure the status bar status is always up to date when the user changes the settings
-    subscriptions.push(vscode.workspace.onDidChangeConfiguration(event => 
+    subscriptions.push(vscode.workspace.onDidChangeConfiguration(event =>
         doActionWhenSettingChangeDetected(event, [wwSetting, wrapColSetting], 
             updateWrapStatusBarItem)));
+
+    //Register the action to do when the user clicks the status bar item
+    vscode.commands.registerCommand(cmdId, handleClick);
 
     console.log(`${extName}: Live status of the WordWrap setting will be shown in the status bar`)
     
     updateWrapStatusBarItem();
+}
+
+//
+// Thanks to medo64 for the double click detection (https://github.com/medo64/Code-Point/blob/main/out/extension.js)
+//
+// Just to point out though, even with the timeout, the 'double-click' seems to be handled twice (once for each click!)
+//  Seems to set the setting correctly though. I think the config is 'read' simultaneously for both handlers
+//
+var dblClickTimerId: ReturnType<typeof setTimeout> | null = null;
+const dblClickTimeMs = 250; //Anything over 250ms is NOT a double click
+
+function handleClick(): void {
+    if (dblClickTimerId == null) {  //If timer does not exist, it's the first click (we're only reponding to double clicks)
+        dblClickTimerId = setTimeout(() => {
+            //We won't do anything for a single click, so the clear timout and do nothing else once the timer expires
+            clearDblClickTimeout();
+        }, dblClickTimeMs);
+    } else {
+        console.debug(`${extName}: Double-click detected`);
+        clearDblClickTimeout();
+        changeWrapSetting();
+    }
+
+    function clearDblClickTimeout() {
+        clearTimeout(dblClickTimerId as ReturnType<typeof setTimeout>);
+        dblClickTimerId = null;
+    }
+
+    function changeWrapSetting(): void {
+        const wrapSettingsOrder = ["on", "off", "wordWrapColumn", "bounded"];
+        const cfg = vscode.workspace.getConfiguration()
+        const origWrapSetting = cfg.get(wwSetting) as string
+        const curSettingElem= wrapSettingsOrder.indexOf(origWrapSetting);
+        const newSetting = wrapSettingsOrder[(curSettingElem+1) % wrapSettingsOrder.length];
+    
+        cfg.update(wwSetting, newSetting, vscode.ConfigurationTarget.Workspace);
+        console.debug(`${extName}: Wrap was '${origWrapSetting}', updating to '${newSetting}'`);    
+    }    
 }
 
 function doActionWhenSettingChangeDetected(event: vscode.ConfigurationChangeEvent, settingToListenFor: string[], action: () => any): void {
